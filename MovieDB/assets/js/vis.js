160b2c94166022f15d145e542ad9f773ded302e8
wklchris <wklchris@hotmail.com>
2018-01-22 05:19:34 -0800

    Milestone #1 for MovieDB. - Color theme update. - Sorting by release date. - Brushbar for zooming view.

--- MovieDB/assets/js/vis.js
+++ MovieDB/assets/js/vis.js
@@ -1,23 +1,28 @@
-var margin = {top: 60, bottom: 60, left: 80, right: 80},
+var margin = {top: 60, bottom: 30, left: 80, right: 80},
+    margin_brush = {top: 120, bottom: 100, left: 80, right: 80},
     width = 0.8*window.innerWidth - margin.left - margin.right,
-    height = 0.8*window.innerHeight - margin.top - margin.bottom;
-
-// Set Axes Scalers
-var xScale = d3.scaleBand()
-        .range([margin.left, width])
-        .padding(0.15);
-var yScale = d3.scaleLinear()
-        .range([height, 0]);
+    height = 0.7*window.innerHeight - margin.top - margin.bottom,
+    height_brush = 50;
 
 // Mouseover & MouseOut Settings
-var hoverColor = "SandyBrown";
+var hoverColor = "Gray";
 
-// Add a SVG element to <body>
+// Add mainGraph/SVG element to <body>
 var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .attr("transform", "translate(0," + margin.top + ")");
-var mainGraph = svg.append("g");
+        .attr("class", "mainSVG")
+        .attr("width", width + margin.left)
+        .attr("height", height + margin.bottom);
+var mainGraph = svg.append("g").attr("class", "mainGraph")
+    .attr("transform", "translate(" + margin.left + ",0)");
+// Add brushGraph/SVG element to <body>
+var brushSvg = d3.select("body").append("svg")
+        .attr("class", "brushSVG")
+        .attr("width", width + margin_brush.left)
+        .attr("height", height_brush);
+var brushGraph = brushSvg.append("g")
+        .attr("class", "brushGraph")
+        .attr("transform", "translate(" + margin_brush.left + ",0)");
+var Ytext = svg.append("text");
 
 //
 // Pop-up Textbox
@@ -35,6 +40,7 @@ function placeXDiv(ori_x, offset_x) {
     }
     return leftside_x;
 }
+
 // Parse complex data from the CSV
 function parseInfo(colname) {
     var target = JSON.parse(currentMovie[colname]);
@@ -55,6 +61,17 @@ function parseDate(s) {
     return s;
 }
 
+// Read Percentiles (pre-processed) for coloring
+var percentiles = {vote_average: [], popularity: [], year: []};
+d3.csv("data/percentiles.csv", function(error, data) {
+    if (error) throw error;
+    data.forEach(function(d) {
+        percentiles.vote_average.push(d.vote_average);
+        percentiles.popularity.push(d.popularity);
+        percentiles.year.push(d.year);
+    })
+});
+
 //
 // Main: Read the CSV & Plotting
 //
@@ -64,6 +81,9 @@ function drawGraph() {
     d3.csv("data/jointtable.csv", function(error, data) {
         if (error) throw error;
 
+        // Detect the radio button selection
+        var colorRadio = d3.select('input[name=colorby]:checked').attr('value');
+
         // Convert strings to numbers
         data.forEach(function(d) {
             d.budget = +d.budget;
@@ -72,63 +92,80 @@ function drawGraph() {
             d.vote_count = +d.vote_count;
             d.popularity = +d.popularity;
         });
-
-        // Detect the radio button selection
-        var colorRadio = d3.select('input[name=colorby]:checked').attr('value');
+        
+        // Filtering & Sorting Data
         if (d3.select('input[name=sorted]').property("checked")) {
-            if (d3.select('input[name=sortby]:checked').attr('value') === "sort_color") {
+            var selectedColumn = d3.select('input[name=sortby]:checked').attr('value');
+            if (selectedColumn === "sort_name") {
+                data.sort(function(a, b) { return d3.ascending(a.title, b.title) });
+            }
+            else if (selectedColumn === "sort_color" || selectedColumn === "year") {
                 data.sort(function(a, b) { return +b[colorRadio] - a[colorRadio]; });
             } else {
                 data.sort(function(a, b) { return +b.revenue - a.revenue; });
             }
         }
-        var vmin = d3.min(data, function(d) { return d[colorRadio]; }),
-            vmax = d3.max(data, function(d) { return d[colorRadio]; });
 
-        // Scaling domains
-        xScale.domain(data.map(function(d) { return d.title; }));
-        yScale.domain([0, d3.max(data, function(d) { return d.revenue; })]);
+        // Set Axes Scalers
+        var xScale = d3.scaleBand()
+            .domain(data.map(function(d) { return d.title; }))
+            .range([0, width]).padding(0.15);
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return d.revenue; })])
+            .range([height, 0]);
 
         // Add bar rectangles
-        mainGraph.selectAll(".bar")
-            .remove().exit()  // Enable update
-            .data(data)
-            .enter().append("rect")
-            .attr("id", function(d, i) { return "rect" + i; })  // d: current datapoint
-            .attr("class", "bar")
-            .attr("fill", function(d) { return barColor(d); })
-            .attr("x", function(d) { return xScale(d.title); })
-            .attr("width", xScale.bandwidth())
-            .attr("y", function(d) { return yScale(d.revenue); })
-            .attr("height", function(d) { return height - yScale(d.revenue); })
-            // Mouse Event
-            .on("mouseover", function(d, i) {
-                d3.select(this).attr("fill", hoverColor);
-                movieInfo(d.movie_id);  // Read Movie Info
-                textDiv.transition().duration(150)
-                    .style("opacity", .9)
-                    .style("background-color", "wheat")
-                    .style("left", placeXDiv(d3.event.pageX, offsets.x) + "px")
-                    .style("top", d3.event.pageY + offsets.y + "px");
-            })
-            .on("mousemove", function(d, i) {
-                if (textDiv.style("opacity") > 0) { 
-                    textDiv.style("left", placeXDiv(d3.event.pageX, offsets.x) + "px")
+        drawBars();
+        function drawBars() {
+            mainGraph.selectAll("rect")
+                // For Updating the Graph
+                .remove().exit()
+                .data(data.filter(function (d) {return xScale.domain().indexOf(d.title) > -1;}))
+                // Add bars
+                .enter().append("rect")
+                .attr("id", function(d, i) { return "rect" + i; })  // d: current datapoint
+                .attr("class", "bar")
+                .attr("fill", function(d) { return barColor(d); })
+                .attr("x", function(d) { return xScale(d.title); })
+                .attr("width", xScale.bandwidth())
+                .attr("y", function(d) { return yScale(d.revenue); })
+                .attr("height", function(d) { return height - yScale(d.revenue); })
+                // Mouse Event
+                .on("mouseover", function(d, i) {
+                    d3.select(this).attr("fill", hoverColor);
+                    movieInfo(d.movie_id);  // Read Movie Info
+                    textDiv.transition().duration(150)
+                        .style("opacity", .9)
+                        .style("background-color", "wheat")
+                        .style("left", placeXDiv(d3.event.pageX, offsets.x) + "px")
                         .style("top", d3.event.pageY + offsets.y + "px");
-                }
-            })
-            .on("mouseout", function(d, i) {
-                textDiv.style("opacity", 0)
-                d3.select(this).transition().duration(300)
-                    .attr("fill", barColor(d));
-                textDiv.selectAll("h1").remove();
-                textDiv.selectAll("p").remove();
-            });
+                })
+                .on("mousemove", function(d, i) {
+                    if (textDiv.style("opacity") > 0) { 
+                        textDiv.style("left", placeXDiv(d3.event.pageX, offsets.x) + "px")
+                            .style("top", d3.event.pageY + offsets.y + "px");
+                    }
+                })
+                .on("mouseout", function(d, i) {
+                    textDiv.style("opacity", 0)
+                    d3.select(this).transition().duration(300)
+                        .attr("fill", barColor(d));
+                    textDiv.selectAll("h1").remove();
+                    textDiv.selectAll("p").remove();
+                });
+        }
         
-        // Color Theme by Radio Button
+        // Color Theme by Radio Button Selection
         function barColor(d) {
-            var colorseq = d3.scaleLinear().domain([vmin, vmax])
-                .range(["lightgray", "black"])
+            var colorseq = d3.scaleThreshold()
+                .domain(//d3.extent(data, function(d) {return d[colorRadio];})
+                    percentiles[colorRadio])
+                .range([
+                        "#F91818", "#F96118", "#F98918",
+                         "#F9A618",  "#F9BD18", 
+                        "#F9D218",  "#F9E818",  "#E5F517",
+                         "#B3EC16",  "#7CE015", "#13C713"
+                    ]); 
             return colorseq(d[colorRadio]);
         }
 
@@ -158,18 +195,64 @@ function drawGraph() {
             textDiv.selectAll("p").attr("class", "tooltip");
         }
 
-        // Add axes
-        var drawYAxis = d3.axisLeft(yScale).tickFormat(d3.format(".2s"));
+        // Draw X-axis brushbar
+        var xScale_brush = d3.scaleBand()
+            .domain(data.map(function(d) { return d.title; }))
+            .range([0, width]).padding(0.15);
+        var yScale_brush = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return d.revenue; })])
+            .range([height_brush, 0]);
+        var brush = d3.brushX().extent([[0, 0], [width, height_brush]])
+            .on("brush", brushed);
+        
+        var brushedBars = brushGraph.selectAll("rect")
+            .remove().exit()
+            .data(data)
+            .enter().append("rect")
+            .attr("fill", function(d) { return barColor(d); })
+            .attr("x", function(d) { return xScale_brush(d.title); })
+            .attr("width", xScale_brush.bandwidth())
+            .attr("y", function(d) { return yScale_brush(d.revenue);})
+            .attr("height", function(d) { return height_brush - yScale_brush(d.revenue); })
+        brushGraph.append("g").call(brush)
+            .call(brush.move, [0, width/120]);  // Brush Initialization
 
-        var zY = svg.append("g")
-            .style("font-size", "16px")
-            .attr("transform", 
-                "translate(" + margin.left + ",0)")
-            .call(drawYAxis);
-        svg.append("text").attr("class", "y-axis")
-            .attr("x", 10)
-            .attr("y", 15)
-            .attr("text-anchor", "right")
-            .text("Revenue");
+        function brushed() {
+            var s = d3.event.selection,
+                selectedBars = {title: [], y: []};
+            data.forEach(function (d) {
+                if (xScale_brush(d.title) > s[0] && 
+                    xScale_brush(d.title) < s[1]) {
+                    selectedBars.title.push(d.title);
+                    selectedBars.y.push(d.revenue);
+                }
+            });
+            // Update domains
+            xScale.domain(selectedBars.title);
+            yScale.domain([0, d3.max(selectedBars.y)]);
+            // Update
+            drawBars();
+            drawAxes();
+        }
+        
+        // Add axes
+        drawAxes();
+        function drawAxes() {
+            svg.selectAll("g.yAxis").remove();
+            var drawYAxis = d3.axisLeft(yScale).tickFormat(d3.format(".2s"));
+            
+            var zY = svg.append("g")
+                .attr("class", "yAxis")
+                .style("font-size", "16px")
+                .attr("transform", 
+                    "translate(" + margin.left + ",0)")
+                .call(drawYAxis);
+            
+            Ytext//.attr("x", 15).attr("y", svg.attr("height") / 2)
+                .attr("text-anchor", "middle")
+                .attr("dy", "0.35em")  // vertical centre the text
+                .attr("transform", "translate(15," + svg.attr("height")/2 + ")rotate(-90)")
+                .text("Revenue");
+        }
     });
 }
