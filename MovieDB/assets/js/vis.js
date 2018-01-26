d3d8afae71dab9f375149e08b658324d0067785f
wklchris <wklchris@hotmail.com>
2018-01-26 01:15:20 -0800

    Re-write visualization with chord plot.

--- MovieDB/assets/js/vis.js
+++ MovieDB/assets/js/vis.js
@@ -1,278 +1,112 @@
-"use strict";
-var margin = {top: 60, bottom: 30, left: 80, right: 80},
-    margin_brush = {top: 120, bottom: 100, left: 80, right: 80},
-    width = 0.8*window.innerWidth - margin.left - margin.right,
-    height = 0.7*window.innerHeight - margin.top - margin.bottom,
-    height_brush = 50, height_dummy = 15;
+// These Data are derived from the preprocess step
+var genres_group = ['Action', 'Adventure', 'Animation', 'Comedy', 'Crime',
+                    'Documentary', 'Drama', 'Family', 'Fantasy', 'Foreign',
+                    'History', 'Horror', 'Music', 'Mystery', 'Romance',
+                    'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western']
+var genres_color = ["#F44336", "#E91E63", "#9C27B0", "#8366B7", "#3F51B5",
+                    "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50", 
+                    "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", 
+                    "#FF5722", "#795548", "#9E9E9E", "#607D8B", "#000000"];
+var genres_flow = [
+    [0,465,26,258,276,3,339,62,144,5,58,76,9,57,63,277,1,547,55,35],
+    [465,0,114,223,56,2,183,211,190,2,27,20,7,37,66,205,0,203,30,22],
+    [26,114,0,125,0,0,19,195,61,1,0,0,14,1,8,30,1,3,1,2],
+    [258,223,125,0,180,11,576,299,166,9,11,78,84,27,484,109,4,113,8,17],
+    [276,56,0,180,0,1,381,8,10,4,13,36,11,105,47,18,1,414,4,9],
+    [3,2,0,11,1,0,7,5,0,2,6,1,15,1,0,0,0,1,0,0],
+    [339,183,19,576,381,7,0,121,99,27,175,84,106,175,603,102,5,554,118,34],
+    [62,211,195,299,8,5,121,0,149,3,1,1,32,6,52,58,3,7,0,3],
+    [144,190,61,166,10,0,99,149,0,0,0,53,11,19,64,85,0,63,3,2],
+    [5,2,1,9,4,2,27,3,0,0,3,0,0,0,9,0,0,3,1,0],
+    [58,27,0,11,13,6,175,1,0,3,0,1,6,3,30,0,0,21,59,8],
+    [76,20,0,78,36,1,84,1,53,0,1,0,3,91,15,95,1,291,1,1],
+    [9,7,14,84,11,15,106,32,11,0,6,3,0,3,61,2,1,5,1,3],
+    [57,37,1,27,105,1,175,6,19,0,3,91,3,0,24,47,0,242,3,2],
+    [63,66,8,484,47,0,603,52,64,9,30,15,61,24,0,31,3,64,26,12],
+    [277,205,30,109,18,0,102,58,85,0,0,95,2,47,31,0,0,211,2,1],
+    [1,0,1,4,1,0,5,3,0,0,0,1,1,0,3,0,0,1,0,0],
+    [547,203,3,113,414,1,554,7,63,3,21,291,5,242,64,211,1,0,24,7],
+    [55,30,1,8,4,0,118,0,3,1,59,1,1,3,26,2,0,24,0,3],
+    [35,22,2,17,9,0,34,3,2,0,8,1,3,2,12,1,0,7,3,0]
+];
 
-// Mouseover & MouseOut Settings
-var hoverColor = "Gray";
+/* **
+** Chord Diagram
+** */
 
-// Add mainGraph/SVG element to <body>
-var svg = d3.select("body").append("svg")
-        .attr("class", "mainSVG")
-        .attr("width", width + margin.left)
-        .attr("height", height + margin.bottom);
-var mainGraph = svg.append("g").attr("class", "mainGraph")
-    .attr("transform", "translate(" + margin.left + ",0)");
-// Add brushGraph/SVG element to <body>
-var brushSvg = d3.select("body").append("svg")
-        .attr("class", "brushSVG")
-        .attr("width", width + margin_brush.left)
-        .attr("height", height_brush);
-var brushGraph = brushSvg.append("g")
-        .attr("class", "brushGraph")
-        .attr("transform", "translate(" + margin_brush.left + ",0)");
-var Ytext = svg.append("text");
-// Add colorBar
-var colorTheme = ["#F94318", "#F97818", "#F99918", "#F9B218", "#F9C818",
-                  "#F9DD18", "#F9F518", "#CBF017", "#9AE716", "#54D715"];
-var colorSvg = d3.select("body").append("svg")
-        .attr("class", "colorBar")
-        .attr("width", width + margin_brush.left)
-        .attr("height", height_brush / 4);
-var colorBar = colorSvg.append("g")
-    .attr("class", "colorBar");
-colorBar.selectAll("rect").data(colorTheme)
-        .enter().append("rect")
-        .attr("x", function(d, i) {
-            return (9-i) * width / 10 + margin_brush.left; 
+var width = {chord: 0.9 * window.innerHeight},
+    height = {chord: 0.9 * window.innerHeight};
+    radius = {chord_outer: width.chord / 2 - 20,
+              chord_inner: width.chord / 2 - 50};
+var svg_chord = d3.select("body").append("svg")
+        .attr("id", "svg_chord")
+        .attr("width", width.chord)
+        .attr("height", height.chord);
+
+// Set the chord layout
+var chord_layout = d3.chord().padAngle(0.03);
+var chord_ribbon = d3.ribbon().radius(radius.chord_inner);
+var chord_graph = svg_chord.append("g")
+        .datum(chord_layout(genres_flow))
+        .attr("transform", "translate(" + width.chord/2 + "," + height.chord/2 + ")");
+// Draw the arc region
+var chord_groups = chord_graph.append("g")
+            .attr("class", "chord_groups")
+        .selectAll("g")
+            .data(function(d) { return d.groups; })
+            .enter().append("g");
+chord_groups.append("path")
+        .style("fill", function(d) { return d3.rgb(genres_color[d.index]); })
+        .style("stroke", "white")
+        .attr("class", "chordArc")
+        .attr("id", function(d) { return "chordArc" + d.index; })
+        .attr("d", d3.arc().innerRadius(radius.chord_inner).outerRadius(radius.chord_outer));
+
+// Draw ticks
+// var chord_ticks = chord_groups.selectAll(".chord_ticks")
+//         .data(function(d) { return setChordTicks(d, 50); } )
+//         .enter().append("g")
+//             .attr("class", "chord_ticks")
+//             .attr("transform", function(d) {
+//                 return "rotate(" + (d.angle * 180 / Math.PI - 90) + ") translate(" + radius.chord_outer + ",0)";
+//             });
+// chord_ticks.append("line").attr("x2", 6);
+// chord_ticks.filter(function(d) { return d.value != 0 && d.value % 400 === 0; })
+//         .append("text")
+//              .attr("x", 8).attr("dy", "0.35em")  // Vertical centre
+//              .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180) translate(-16)" : null })
+//              .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+//              .text(function(d) { return d.value; });
+
+// function setChordTicks(arcs, step) {
+//     var gap = (arcs.endAngle - arcs.startAngle) / arcs.value;
+//     return d3.range(0, arcs.value, step).map(function(d) {
+//         return {value: d, angle: +arcs.startAngle + (arcs.endAngle - arcs.startAngle) / arcs.value * d};
+//     });
+// }
+
+// Draw ribbons
+chord_graph.append("g").attr("class", "chord_ribbons")
+        .selectAll("path")
+        .data(function(d) { return d; }).enter()
+    .append("path")
+        .attr("class", "chordRibbon")
+        .attr("source", function(d) { return d.source.index; })
+        .attr("id", function(d) { return "chordRibbon" + d.source.index + "-" + d.target.index; })
+        .attr("d", chord_ribbon)
+        .style("fill", function(d) {
+            var fillRibbons = d3.interpolateRgb(genres_color[d.source.index], genres_color[d.target.index]);
+            return fillRibbons(0.5);
         })
-        .attr("y", 0)
-        .attr("width", width / 10).attr("height", height_brush / 4)
-        .attr("fill", function(d) { return d; })
-        .attr("display", "none");
-//
-// Pop-up Textbox
-//
-var textDiv = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .attr("opacity", 0);
-var offsets = {x: 50, y: -100};
-var currentMovie = {};
-// Where to put the pop-up textbox
-function placeXDiv(ori_x, offset_x) {
-    var leftside_x = ori_x - offset_x - textDiv.node().offsetWidth
-    if (leftside_x < margin.left) {
-        return ori_x + offset_x;
-    }
-    return leftside_x;
-}
+        .style("stroke", "lightgray")
 
-// Parse complex data from the CSV
-function parseInfo(colname) {
-    var target = [{name: "N/A"}];
-    var s = [];
-    try{
-        target = JSON.parse(currentMovie[colname]);
-    } catch(e) { throw e; }
-    target.forEach(function(d) {s.push(d.name);})
-    s.sort();
-    return s.join(", ") + ". ";
-}
-
-// Set Date Format
-function parseDate(s) {
-    if (s.indexOf("/") >= 0) {
-        var dates = s.split("/");  // Raw format: mm/dd/YYYY
-        if (+dates[0] < 10) {dates[0] = "0" + dates[0]}
-        if (+dates[1] < 10) {dates[1] = "0" + dates[1]}
-        return [dates[2], dates[0], dates[1]].join("-");
-    }
-    return s;
-}
-
-// Read Percentiles (pre-processed) for coloring
-var percentiles = {vote_average: [], popularity: [], year: []};
-d3.csv("data/percentiles.csv", function(error, data) {
-    if (error) throw error;
-    data.forEach(function(d) {
-        percentiles.vote_average.push(d.vote_average);
-        percentiles.popularity.push(d.popularity);
-        percentiles.year.push(d.year);
-    })
-});
-
-//
-// Main: Read the CSV & Plotting
-//
-drawGraph();
-
-function drawGraph() {
-    d3.csv("data/jointtable.csv", function(error, data) {
-        if (error) throw error;
-
-        // Detect the radio button selection
-        var colorRadio = d3.select('input[name=colorby]:checked').attr('value');
-
-        // Convert strings to numbers
-        data.forEach(function(d) {
-            d.budget = +d.budget;
-            d.revenue = +d.revenue;
-            d.vote_average = +d.vote_average;
-            d.vote_count = +d.vote_count;
-            d.popularity = +d.popularity;
-        });
-        
-        // Filtering & Sorting Data
-        if (d3.select('input[name=sorted]').property("checked")) {
-            var selectedColumn = d3.select('input[name=sortby]:checked').attr('value');
-            if (selectedColumn === "sort_name") {
-                data.sort(function(a, b) { return d3.ascending(a.title, b.title) });
-            }
-            else if (selectedColumn === "sort_color" || selectedColumn === "year") {
-                data.sort(function(a, b) { return +b[colorRadio] - a[colorRadio]; });
-            } else {
-                data.sort(function(a, b) { return +b.revenue - a.revenue; });
-            }
-        }
-
-        // Set Axes Scalers
-        var xScale = d3.scaleBand()
-            .domain(data.map(function(d) { return d.title; }))
-            .range([0, width]).padding(0.15);
-        var yScale = d3.scaleLinear()
-            .domain([0, d3.max(data, function(d) { return d.revenue; })])
-            .range([height, 0]); 
-
-        // Add bar rectangles
-        drawBars();
-        function drawBars() {
-            mainGraph.selectAll("rect").remove().exit()
-                .data(data.filter(function (d) { return xScale.domain().indexOf(d.title) > -1; }))
-                // Add bars
-                .enter().append("rect")
-                .attr("id", function(d, i) { return "rect" + i; })
-                .attr("class", "bar")
-                .attr("fill", function(d) { return barColor(d); })
-                .attr("x", function(d) { return xScale(d.title); })
-                .attr("width", xScale.bandwidth())
-                .attr("y", function(d) { return yScale(d.revenue); })
-                .attr("height", function(d) { return height - yScale(d.revenue) + height_dummy; })
-                // Mouse Event
-                .on("mouseover", handleMouseOver)
-                .on("mousemove", handleMouseMove)
-                .on("mouseout", handleMouseOut);
-        }
-        
-        // Color Theme by Radio Button Selection
-        function barColor(d) {
-            var colorseq = d3.scaleThreshold()
-                .domain(percentiles[colorRadio])
-                .range(colorTheme); 
-            return colorseq(d[colorRadio]);
-        }
-
-        // Mouse Events
-        function handleMouseOver(d, i) {
-            d3.select(this).attr("fill", hoverColor);
-            movieInfo(d.movie_id);  // Read Movie Info
-            textDiv.transition().duration(150)
-                .style("opacity", .9)
-                .style("background-color", "wheat")
-                .style("left", placeXDiv(d3.event.pageX, offsets.x) + "px")
-                .style("top", d3.event.pageY + offsets.y + "px");
-        }
-
-        function handleMouseMove(d, i) {
-            if (textDiv.style("opacity") > 0) { 
-                textDiv.style("left", placeXDiv(d3.event.pageX, offsets.x) + "px")
-                    .style("top", d3.event.pageY + offsets.y + "px");
-            }
-        }
-
-        function handleMouseOut(d, i) {
-            textDiv.style("opacity", 0)
-            d3.select(this).transition().duration(300)
-                .attr("fill", barColor(d));
-            textDiv.selectAll("h1").remove();
-            textDiv.selectAll("p").remove(); 
-        }
-
-        // Display information of movie with id = #k
-        function movieInfo(k) {
-            var tempData = data.filter(function (row) {
-                return row["movie_id"] == k;
-            });
-            currentMovie = tempData[0];
-            // Add text to the textbox
-            textDiv.append("h1").text(currentMovie.title)
-                .attr("class", "tooltip");
-            textDiv.append("p").html("<b>Genres:</b> " + parseInfo("genres"));
-            textDiv.append("p").html('<b>Year:</b> '
-                + parseDate(currentMovie.release_date) + ", <b>Popularity:</b> "
-                + Math.round(+currentMovie.popularity * 100) / 100);
-            var dolarFormat = d3.format(",.4r");
-            textDiv.append("p").html("<b>Budget:</b>  "
-                + dolarFormat(currentMovie.budget));
-            textDiv.append("p").html("<b>Revenue:</b> "
-                + dolarFormat(currentMovie.revenue));
-            textDiv.append("p").html("<b>Rate:</b> " + currentMovie.vote_average
-                + " (" + currentMovie.vote_count + " votes)");
-            // Add class for all these <p>
-            textDiv.selectAll("p").attr("class", "tooltip");
-        }
-
-        // Draw X-axis brushbar
-        var xScale_brush = d3.scaleBand()
-            .domain(data.map(function(d) { return d.title; }))
-            .range([0, width]).padding(0.15);
-        var yScale_brush = d3.scaleLinear()
-            .domain([0, d3.max(data, function(d) { return d.revenue; })])
-            .range([height_brush, 0]);
-        var brush = d3.brushX().extent([[0, 0], [width, height_brush]])
-            .on("brush", brushed);
-        
-        var brushedBars = brushGraph.selectAll("rect")
-            .remove().exit()
-            .data(data)
-            .enter().append("rect")
-            .attr("fill", function(d) { return barColor(d); })
-            .attr("x", function(d) { return xScale_brush(d.title); })
-            .attr("width", xScale_brush.bandwidth())
-            .attr("y", function(d) { return yScale_brush(d.revenue);})
-            .attr("height", function(d) { return height_brush - yScale_brush(d.revenue); })
-        brushGraph.call(brush)
-            .call(brush.move, [0, width/120]);  // Brush Initialization
-
-        function brushed() {
-            var s = d3.event.selection,
-                selectedBars = {title: [], y: []};
-            data.forEach(function (d) {
-                if (xScale_brush(d.title) > s[0] && xScale_brush(d.title) < s[1]) {
-                    selectedBars.title.push(d.title);
-                    selectedBars.y.push(d.revenue);
-                }
-            });
-            // Update domains
-            xScale.domain(selectedBars.title);
-            yScale.domain([0, d3.max(selectedBars.y)]);
-            // Update Graph
-            drawBars();
-            drawAxes();
-        }
-        
-        // Add axes
-        drawAxes(); 
-        function drawAxes() {
-            svg.selectAll("g.yAxis").remove();
-            var drawYAxis = d3.axisLeft(yScale).tickFormat(d3.format(".2s"));
-            
-            var zY = svg.append("g")
-                .attr("class", "yAxis")
-                .style("font-size", "16px")
-                .attr("transform", 
-                    "translate(" + margin.left + ",0)")
-                .call(drawYAxis);
-            
-            Ytext.attr("text-anchor", "middle")
-                .attr("dy", "0.35em")  // vertical centre the text
-                .attr("transform", "translate(15," + svg.attr("height")/2 + ")rotate(-90)")
-                .text("Revenue");
-        }
-
-        // Display colorbar
-        colorBar.selectAll("rect").attr("display", "inline");
-    });
-}
-    
+// Define mouse events
+d3.selectAll(".chordArc")
+        .on("mouseover", function(d, i) {
+            //d3.selectAll(".chordArc:not(#" + this.id + ")").attr("visibility", "hidden");
+            d3.selectAll(".chordRibbon").filter(function(x) { return x.source.index != i && x.target.index != i; })
+                .attr("visibility", "hidden");
+        })
+        .on("mouseout", function(d, i) {
+            d3.selectAll(".chordRibbon").attr("visibility", "visible");
+        })
--- ReadMe.md
+++ ReadMe.md
@@ -6,11 +6,11 @@ I planned to build a bilingual webpage for this project.
 ## In English
 
 This is a repo for my visualizations, including:
-- **SHMetro**: An interactive Shanghai subway network implemented by D3.js.
-- **MovieDB**: An interactive "barchart" by D3.js. 
+- **SHMetro**: An interactive Shanghai subway network implemented by D3.js. 
+- **MovieDB**: An interactive project about MovieDB by D3.js. [Link](https://wklchris.github.io/Visual-Projects/MovieDB/MovieDB.html)
 
 ## 中文
 
 这是存放我个人的可视化项目的仓库，包括：
 - **上海地铁**：用 D3.js 实现的、可交互的上海地铁网络图。
-- **电影数据库**：用 D3.js 实现的、可交互的电影数据“条形图”。
+- **电影数据库**：用 D3.js 实现的、可交互的电影数据图。[链接](https://wklchris.github.io/Visual-Projects/MovieDB/MovieDB.html)
